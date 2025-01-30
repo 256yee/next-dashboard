bf9010db48900cda601b5b5ca6035485b8f9ec4b
256yee <256yee@gmail.com>
2025-01-30 22:48:35 +0800

    Update project structure

--- app/ui/invoices/pagination.tsx
+++ app/ui/invoices/pagination.tsx
@@ -1,20 +1,34 @@
-'use client';
+"use client";
 
-import { ArrowLeftIcon, ArrowRightIcon } from '@heroicons/react/24/outline';
-import clsx from 'clsx';
-import Link from 'next/link';
-import { generatePagination } from '@/app/lib/utils';
+import { ArrowLeftIcon, ArrowRightIcon } from "@heroicons/react/24/outline";
+import clsx from "clsx";
+import Link from "next/link";
+import { generatePagination } from "@/app/lib/utils";
+import { usePathname, useSearchParams } from "next/navigation";
 
 export default function Pagination({ totalPages }: { totalPages: number }) {
   // NOTE: Uncomment this code in Chapter 11
 
-  // const allPages = generatePagination(currentPage, totalPages);
+  const pathname = usePathname(); // // 获取当前路径（不含查询参数）。
+  const searchParams = useSearchParams(); // 获取当前查询参数（不含路径）。
+  const currentPage = Number(searchParams.get("page")) || 1; // 获取当前页码（默认为 1）。
+  const allPages = generatePagination(currentPage, totalPages); // 返回一个数组，例如 [1, "...", 3, 4, 5, "...", 10]，用于显示分页按钮。
+
+  // ↓这个函数修改 URL 中的 page 参数，生成新的分页链接
+  const createPageURL = (pageNumber: number | string) => {
+    // ↓复制当前URL的查询参数，以便我们可以修改它而不影响原始URL
+    const params = new URLSearchParams(searchParams);
+    // ↓设置查询参数中的页面编号为指定的值
+    params.set("page", pageNumber.toString());
+    // ↓构造并返回包含新页面编号查询参数的完整URL
+    return `${pathname}?${params.toString()}`;
+  };
 
   return (
     <>
       {/*  NOTE: Uncomment this code in Chapter 11 */}
 
-      {/* <div className="inline-flex">
+      <div className="inline-flex">
         <PaginationArrow
           direction="left"
           href={createPageURL(currentPage - 1)}
@@ -23,12 +37,12 @@ export default function Pagination({ totalPages }: { totalPages: number }) {
 
         <div className="flex -space-x-px">
           {allPages.map((page, index) => {
-            let position: 'first' | 'last' | 'single' | 'middle' | undefined;
+            let position: "first" | "last" | "single" | "middle" | undefined;
 
-            if (index === 0) position = 'first';
-            if (index === allPages.length - 1) position = 'last';
-            if (allPages.length === 1) position = 'single';
-            if (page === '...') position = 'middle';
+            if (index === 0) position = "first";
+            if (index === allPages.length - 1) position = "last";
+            if (allPages.length === 1) position = "single";
+            if (page === "...") position = "middle";
 
             return (
               <PaginationNumber
@@ -47,7 +61,7 @@ export default function Pagination({ totalPages }: { totalPages: number }) {
           href={createPageURL(currentPage + 1)}
           isDisabled={currentPage >= totalPages}
         />
-      </div> */}
+      </div>
     </>
   );
 }
@@ -60,21 +74,21 @@ function PaginationNumber({
 }: {
   page: number | string;
   href: string;
-  position?: 'first' | 'last' | 'middle' | 'single';
+  position?: "first" | "last" | "middle" | "single";
   isActive: boolean;
 }) {
   const className = clsx(
-    'flex h-10 w-10 items-center justify-center text-sm border',
+    "flex h-10 w-10 items-center justify-center text-sm border",
     {
-      'rounded-l-md': position === 'first' || position === 'single',
-      'rounded-r-md': position === 'last' || position === 'single',
-      'z-10 bg-blue-600 border-blue-600 text-white': isActive,
-      'hover:bg-gray-100': !isActive && position !== 'middle',
-      'text-gray-300': position === 'middle',
-    },
+      "rounded-l-md": position === "first" || position === "single",
+      "rounded-r-md": position === "last" || position === "single",
+      "z-10 bg-blue-600 border-blue-600 text-white": isActive,
+      "hover:bg-gray-100": !isActive && position !== "middle",
+      "text-gray-300": position === "middle",
+    }
   );
 
-  return isActive || position === 'middle' ? (
+  return isActive || position === "middle" ? (
     <div className={className}>{page}</div>
   ) : (
     <Link href={href} className={className}>
@@ -89,21 +103,21 @@ function PaginationArrow({
   isDisabled,
 }: {
   href: string;
-  direction: 'left' | 'right';
+  direction: "left" | "right";
   isDisabled?: boolean;
 }) {
   const className = clsx(
-    'flex h-10 w-10 items-center justify-center rounded-md border',
+    "flex h-10 w-10 items-center justify-center rounded-md border",
     {
-      'pointer-events-none text-gray-300': isDisabled,
-      'hover:bg-gray-100': !isDisabled,
-      'mr-2 md:mr-4': direction === 'left',
-      'ml-2 md:ml-4': direction === 'right',
-    },
+      "pointer-events-none text-gray-300": isDisabled,
+      "hover:bg-gray-100": !isDisabled,
+      "mr-2 md:mr-4": direction === "left",
+      "ml-2 md:ml-4": direction === "right",
+    }
   );
 
   const icon =
-    direction === 'left' ? (
+    direction === "left" ? (
       <ArrowLeftIcon className="w-4" />
     ) : (
       <ArrowRightIcon className="w-4" />
--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,8 +1,27 @@
-'use client';
+"use client";
 
-import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
+import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
+import { useSearchParams, usePathname, useRouter } from "next/navigation";
+import { useDebouncedCallback } from "use-debounce"; // 去抖动的包
 
 export default function Search({ placeholder }: { placeholder: string }) {
+  const searchParams = useSearchParams(); // 获取当前 URL 的查询参数（?query=something）
+  const pathname = usePathname(); // 获取当前路径（不含查询参数）。
+  const { replace } = useRouter(); // 用于导航，replace 方法可以替换当前 URL
+
+  // handleSearch 是一个防抖函数，当用户输入时，它会等待 300 毫秒再执行搜索逻辑
+  const handleSearch = useDebouncedCallback((term) => {
+    // ↓useSearchParams() 获取的是 不可变（immutable）的查询参数对象，所以不能直接修改它。所以用 new URLSearchParams(searchParams) 创建一个 可修改 的副本。
+    const params = new URLSearchParams(searchParams); // URLSearchParams 是 JavaScript 内置的 Web API，用于操作 URL 查询参数（?key=value 部分）。
+    params.set("page", "1"); // 搜索后，重置到第 1 页
+    if (term) {
+      params.set("query", term); // 如果用户输入了内容，它会更新 URL 的 query 参数
+    } else {
+      params.delete("query"); // 如果输入框为空，则移除 query 参数
+    }
+    replace(`${pathname}?${params.toString()}`); // 通过 replace 方法更新浏览器的 URL。
+  }, 300);
+  
   return (
     <div className="relative flex flex-1 flex-shrink-0">
       <label htmlFor="search" className="sr-only">
@@ -10,7 +29,12 @@ export default function Search({ placeholder }: { placeholder: string }) {
       </label>
       <input
         className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
-        placeholder={placeholder}
+        placeholder={placeholder} //输入框的 placeholder 属性由外部传入
+        // ↓当用户输入内容时，onChange 事件会触发 handleSearch 函数
+        onChange={(e) => {
+          handleSearch(e.target.value);
+        }}
+        defaultValue={searchParams.get("query")?.toString()} // 反过来，输入框的默认值（defaultValue）从 URL 的 query 参数中获取
       />
       <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
     </div>
